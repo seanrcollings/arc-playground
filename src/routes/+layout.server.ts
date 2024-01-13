23d4a3a8939aa5b2417b1156bc7846c13f51ebce
Sean Collings <seanrcollings@gmail.com>
2024-01-13 15:20:22 -0700

    Have a fallback if it can't get the examples

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,11 +6,11 @@ export async function load({ fetch }) {
   try {
     examples = await getExampleIndex(fetch);
   } catch (e) {
+    // In case of error, we don't want to crash the app, so we'll just
+    // return a single example
     console.error(e);
     examples = [FALLBACK_EXAMPLE];
   }
 
-  return {
-    examples: await getExampleIndex(fetch),
-  };
+  return { examples };
 }
